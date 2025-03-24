1ce107b36fb015783859a0071fb5be578e039747
Vivek <vivekrajput2515@gmail.com>
2025-03-24 18:42:31 +0530

    add hash function

--- src/user/userController.ts
+++ src/user/userController.ts
@@ -1,7 +1,7 @@
 import { Request, Response, NextFunction } from "express";
 import createHttpError from "http-errors";
 import userModel from "./userModel";
-
+import bcrypt from "bcrypt";
 const createUser = async(req:Request,res:Response,next:NextFunction)=>{
 
     const {name,email,password} = req.body;
@@ -18,6 +18,9 @@ const createUser = async(req:Request,res:Response,next:NextFunction)=>{
         const error = createHttpError(400,"User Already exists");
         return next(error);
     }
+
+    //password -> hashed
+    const hashedPassword = await bcrypt.hash(password,10);
     //process
     //response
 
