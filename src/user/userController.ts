77726e00871a66a3eb72fb93ec09eada622811d5
Vivek <vivekrajput2515@gmail.com>
2025-03-24 22:28:36 +0530

    sign jwt token

--- src/user/userController.ts
+++ src/user/userController.ts
@@ -2,6 +2,9 @@ import { Request, Response, NextFunction } from "express";
 import createHttpError from "http-errors";
 import userModel from "./userModel";
 import bcrypt from "bcrypt";
+import { sign } from "jsonwebtoken";
+import {config} from "../config/config"
+
 const createUser = async(req:Request,res:Response,next:NextFunction)=>{
 
     const {name,email,password} = req.body;
@@ -21,12 +24,21 @@ const createUser = async(req:Request,res:Response,next:NextFunction)=>{
 
     //password -> hashed
     const hashedPassword = await bcrypt.hash(password,10);
+
+    const newUser = await userModel.create({
+        name,
+        email,
+        password: hashedPassword,
+    });
+
+    //token generation jwt
+    const token = sign({sub:newUser._id},config.jwtSecret as string, {expiresIn: '7d'})
     //process
     //response
 
     res.json({
-        message: "User Created"
-    })
+        accessTokesn: token
+    });
 };
 
 export {createUser};
