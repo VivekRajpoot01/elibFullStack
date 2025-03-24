9c90e4fc55791678a7e29886228608ba87b048ec
Vivek <vivekrajput2515@gmail.com>
2025-03-24 17:38:46 +0530

    add global error handler

--- src/app.ts
+++ src/app.ts
@@ -1,10 +1,31 @@
-import express from 'express'
-
+import express, {Request,Response,NextFunction} from 'express'
+import createHttpError ,{ HttpError } from 'http-errors'
+import {config} from "./config/config"
 const app = express()
 
 
 app.get("/",(req,res,next)=>{
+
+    
+    // const error = createHttpError(400, "something went wrong")
+    // throw error;
+
     res.json({"message":"Welcome to our elib apis"})
+
 })
 
+//global error handler
+
+
+
+ /* 
+app.use((err: HttpError, req: Request, res: Response, next: NextFunction) => {
+    const statusCode = err.statusCode || 500;
+  
+    return res.status(statusCode).json({
+      message: err.message,
+      errorStack: config.env === 'development' ? err.stack || '' : '',
+    })
+  });
+*/
 export default app;
